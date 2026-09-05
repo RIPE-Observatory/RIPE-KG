770e27dbb9a9d8d5484cb20b88e7bbaa622ecd60
Goutham Indukuri <i.gouthamvarma@gmail.com>
2026-09-05 15:55:12 +0100

    Migrate entity pages to version links

--- ui/src/app/assessments/[uuid]/page.tsx
+++ ui/src/app/assessments/[uuid]/page.tsx
@@ -1,5 +1,5 @@
 import { notFound } from "next/navigation";
-import Link from "next/link";
+import Link from "@/components/version-link";
 import type { Metadata } from "next";
 import {
   fetchAssessmentHeader,
--- ui/src/app/publications/[doi]/page.tsx
+++ ui/src/app/publications/[doi]/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Link from "@/components/version-link";
 import { notFound } from "next/navigation";
 import type { Metadata } from "next";
 import type { ReactNode } from "react";
